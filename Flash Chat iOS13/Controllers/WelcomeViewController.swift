9fc2e7b937165d62d5a49f7b85775c96f6402ca7
Sudhar San <34421911+SudharsanK58@users.noreply.github.com>
2023-05-26 16:46:04 +0530

    Learning started

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -15,8 +15,18 @@ class WelcomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-       
+        titleLabel.text = "⚡️ZusanChat"
+        animateTitleLabel()
     }
     
-
+    private func animateTitleLabel() {
+        titleLabel.transform = CGAffineTransform(scaleX: 0.1, y: 0.1)
+        titleLabel.alpha = 0.0
+        
+        UIView.animate(withDuration: 3.0, delay: 0.0, usingSpringWithDamping: 0.6, initialSpringVelocity: 0.1, options: .curveEaseInOut, animations: {
+            self.titleLabel.transform = CGAffineTransform.identity
+            self.titleLabel.alpha = 1.0
+        }, completion: nil)
+    }
 }
+
