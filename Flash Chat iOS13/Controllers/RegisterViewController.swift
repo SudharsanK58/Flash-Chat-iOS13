ae4581addd6f0ee9ead9ffe638675466b66aa870
Sudhar San <34421911+SudharsanK58@users.noreply.github.com>
2023-05-31 19:25:24 +0530

    Added alerts for email and password validation

--- Flash Chat iOS13/Controllers/RegisterViewController.swift
+++ Flash Chat iOS13/Controllers/RegisterViewController.swift
@@ -7,13 +7,43 @@
 //
 
 import UIKit
+import FirebaseCore
+import FirebaseFirestore
+import FirebaseAuth
 
+      
 class RegisterViewController: UIViewController {
 
     @IBOutlet weak var emailTextfield: UITextField!
     @IBOutlet weak var passwordTextfield: UITextField!
     
     @IBAction func registerPressed(_ sender: UIButton) {
+        if let email = emailTextfield.text , let password =  passwordTextfield.text{
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                // ...
+                if let err = error{
+                    print(err.localizedDescription)
+                    self.showAlert(message: err.localizedDescription)
+                }else{
+                    self.performSegue(withIdentifier: "registerToChat", sender: self)
+                }
+            }
+        }
+    }
+    
+    func showAlert(message: String) {
+        let alertController = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "OK", style: .default) { (_) in
+            // Handle OK button action
+            print("OK button tapped")
+        }
+        
+        alertController.addAction(okAction)
+        
+        if let rootViewController = UIApplication.shared.windows.first?.rootViewController {
+            rootViewController.present(alertController, animated: true, completion: nil)
+        }
     }
     
 }
