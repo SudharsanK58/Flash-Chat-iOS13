e5539f69558d93e64c9a15213f1bc40672788862
Sudhar San <34421911+SudharsanK58@users.noreply.github.com>
2023-05-31 15:46:09 +0530

    Added mqtt for learning
    A

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -1,32 +1,152 @@
-//
-//  WelcomeViewController.swift
-//  Flash Chat iOS13
-//
-//  Created by Angela Yu on 21/10/2019.
-//  Copyright © 2019 Angela Yu. All rights reserved.
-//
-
 import UIKit
+import CocoaMQTT
 
 class WelcomeViewController: UIViewController {
-
-    @IBOutlet weak var titleLabel: UILabel!
-    
+    let defaultHost = "mqtt.zig-web.com"
+    var mqtt5: CocoaMQTT5?
+    var animal: String?
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        titleLabel.text = "⚡️ZusanChat"
-        animateTitleLabel()
+        let clientID = "CocoaMQTT5" + String(ProcessInfo().processIdentifier)
+        mqtt5 = CocoaMQTT5(clientID: clientID, host: defaultHost, port: 1883)
+        //        mqtt5!.logLevel = .debug
+        let connectProperties = MqttConnectProperties()
+        connectProperties.topicAliasMaximum = 0
+        connectProperties.sessionExpiryInterval = 0
+        connectProperties.receiveMaximum = 100
+        connectProperties.maximumPacketSize = 500
+        
+        mqtt5!.connectProperties = connectProperties
+        mqtt5!.username = "Android"
+        mqtt5!.password = "Polgara12ZED2122"
+        
+        let lastWillMessage = CocoaMQTT5Message(topic: "/will", string: "dieout")
+        lastWillMessage.contentType = "JSON"
+        lastWillMessage.willResponseTopic = "/will"
+        lastWillMessage.willExpiryInterval = .max
+        lastWillMessage.willDelayInterval = 0
+        lastWillMessage.qos = .qos1
+        
+        mqtt5!.willMessage = lastWillMessage
+        mqtt5!.keepAlive = 60
+        mqtt5!.delegate = self
+        mqtt5?.connect()
     }
     
-    private func animateTitleLabel() {
-        titleLabel.transform = CGAffineTransform(scaleX: 0.1, y: 0.1)
-        titleLabel.alpha = 0.0
+}
+extension WelcomeViewController: CocoaMQTT5Delegate {
+    
+    func mqtt5(_ mqtt5: CocoaMQTT5, didReceiveDisconnectReasonCode reasonCode: CocoaMQTTDISCONNECTReasonCode) {
+        print("disconnect res : \(reasonCode)")
+    }
+    
+    func mqtt5(_ mqtt5: CocoaMQTT5, didReceiveAuthReasonCode reasonCode: CocoaMQTTAUTHReasonCode) {
+        print("auth res : \(reasonCode)")
+    }
+    
+    // Optional ssl CocoaMQTT5Delegate
+    func mqtt5(_ mqtt5: CocoaMQTT5, didReceive trust: SecTrust, completionHandler: @escaping (Bool) -> Void) {
+        TRACE("trust: \(trust)")
+        completionHandler(true)
+    }
+    
+    func mqtt5(_ mqtt5: CocoaMQTT5, didConnectAck ack: CocoaMQTTCONNACKReasonCode, connAckData: MqttDecodeConnAck?) {
+        TRACE("ack: \(ack)")
         
-        UIView.animate(withDuration: 3.0, delay: 0.0, usingSpringWithDamping: 0.6, initialSpringVelocity: 0.1, options: .curveEaseInOut, animations: {
-            self.titleLabel.transform = CGAffineTransform.identity
-            self.titleLabel.alpha = 1.0
-        }, completion: nil)
+        if ack == .success {
+            if(connAckData != nil){
+                print("properties maximumPacketSize: \(String(describing: connAckData!.maximumPacketSize))")
+                print("properties topicAliasMaximum: \(String(describing: connAckData!.topicAliasMaximum))")
+            }
+            mqtt5.subscribe("98:CD:AC:51:4A:00/device", qos: CocoaMQTTQoS.qos0)
+        }
+    }
+    
+    
+    func mqtt5(_ mqtt5: CocoaMQTT5, didStateChangeTo state: CocoaMQTTConnState) {
+        TRACE("new state: \(state)")
+        if state == .disconnected {
+            
+        }
+    }
+    
+    func mqtt5(_ mqtt5: CocoaMQTT5, didPublishMessage message: CocoaMQTT5Message, id: UInt16) {
+        TRACE("message: \(message.description), id: \(id)")
+    }
+    
+    func mqtt5(_ mqtt5: CocoaMQTT5, didPublishAck id: UInt16, pubAckData: MqttDecodePubAck?) {
+        TRACE("id: \(id)")
+        if(pubAckData != nil){
+            print("pubAckData reasonCode: \(String(describing: pubAckData!.reasonCode))")
+        }
+    }
+    
+    func mqtt5(_ mqtt5: CocoaMQTT5, didPublishRec id: UInt16, pubRecData: MqttDecodePubRec?) {
+        TRACE("id: \(id)")
+        if(pubRecData != nil){
+            print("pubRecData reasonCode: \(String(describing: pubRecData!.reasonCode))")
+        }
+    }
+    
+    func mqtt5(_ mqtt5: CocoaMQTT5, didPublishComplete id: UInt16,  pubCompData: MqttDecodePubComp?){
+        TRACE("id: \(id)")
+        if(pubCompData != nil){
+            print("pubCompData reasonCode: \(String(describing: pubCompData!.reasonCode))")
+        }
+    }
+    
+    func mqtt5(_ mqtt5: CocoaMQTT5, didReceiveMessage message: CocoaMQTT5Message, id: UInt16, publishData: MqttDecodePublish?){
+        if(publishData != nil){
+            print("publish.contentType \(String(describing: publishData!.contentType))")
+        }
+        
+        //        TRACE("message: \(message.description), id: \(id)")
+        print("+++++++++Topic+++++++++")
+        print(message.topic)
+        print("+++++++++Payload+++++++++")
+        print(message.string!.description)
+        print("+++++++++Description+++++++++")
+        TRACE("message: \(message.string!.description), id: \(id)")
+    }
+    
+    func mqtt5(_ mqtt5: CocoaMQTT5, didSubscribeTopics success: NSDictionary, failed: [String], subAckData: MqttDecodeSubAck?) {
+        TRACE("subscribed: \(success), failed: \(failed)")
+        if(subAckData != nil){
+            print("subAckData.reasonCodes \(String(describing: subAckData!.reasonCodes))")
+        }
+    }
+    
+    func mqtt5(_ mqtt5: CocoaMQTT5, didUnsubscribeTopics topics: [String], UnsubAckData: MqttDecodeUnsubAck?) {
+    }
+    
+    func mqtt5DidPing(_ mqtt5: CocoaMQTT5) {
+        TRACE()
+    }
+    
+    func mqtt5DidReceivePong(_ mqtt5: CocoaMQTT5) {
+        TRACE()
+    }
+    
+    func mqtt5DidDisconnect(_ mqtt5: CocoaMQTT5, withError err: Error?) {
+        let name = NSNotification.Name(rawValue: "MQTTMessageNotificationDisconnect")
+        NotificationCenter.default.post(name: name, object: nil)
     }
 }
 
+extension WelcomeViewController {
+    func TRACE(_ message: String = "", fun: String = #function) {
+        let names = fun.components(separatedBy: ":")
+        var prettyName: String
+        if names.count == 2 {
+            prettyName = names[0]
+        } else {
+            prettyName = names[1]
+        }
+        
+        if fun == "mqttDidDisconnect(_:withError:)" {
+            prettyName = "didDisconnect"
+        }
+        
+        print("[TRACE] [\(prettyName)]: \(message)")
+    }
+}
